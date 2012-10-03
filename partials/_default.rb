692be728e1e18eee73ba315377c5528844dbe8af
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-03 20:36:55 -0300

    Added partial for flash messages (closes #10)

--- partials/_default.rb
+++ partials/_default.rb
@@ -1,17 +1,28 @@
 puts "Adding default stuff ...".magenta
 
+copy_static_file 'app/views/layouts/_flash.html.slim'
+git :add => '.'
+git :commit => "-aqm 'Added partial for flash messages'"
+
+
+
 copy_static_file 'app/views/layouts/application.html.slim'
 gsub_file 'app/views/layouts/application.html.slim', /PROJECT/, @app_name
 git :add => '.'
-git :commit => "-aqm 'Add application layout in slim.'"
+git :commit => "-aqm 'Added application layout in slim'"
+
+
 
 copy_static_file 'config/locales/rails.pt-BR.yml'
 git :add => '.'
-git :commit => "-aqm 'Add rails locale for pt-BR.'"
+git :commit => "-aqm 'Added rails locale for pt-BR'"
+
+
 
 copy_static_file '.gitignore'
 git :add => '.'
-git :commit => "-aqm 'Add .gitignore'"
+git :commit => "-aqm 'Added .gitignore'"
+
 
 
 gsub_file 'app/controllers/application_controller.rb', "end", "
@@ -22,5 +33,5 @@ gsub_file 'app/controllers/application_controller.rb', "end", "
 end
 "
 git :add => '.'
-git :commit => "-aqm 'Add render_404 to application controller'"
+git :commit => "-aqm 'Added render_404 to application controller'"
 puts "\n"
