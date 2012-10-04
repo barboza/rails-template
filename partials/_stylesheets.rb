6ea8fda97ad095407696d231ec7f54f6b216c74b
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-03 23:06:46 -0300

    Commit bootstrap only if would_you_like?

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -8,9 +8,8 @@ git :commit => "-aqm 'Renamed application.css to application.sass'"
 
 if would_you_like? "Use twitter bootstrap? [y,n]".red
   gsub_file 'Gemfile', /#gem 'bootstrap-sass/, "gem 'bootstrap-sass"
+  run "echo '\n@import bootstrap' >> app/assets/stylesheets/application.sass"
+  git :add => '.'
+  git :commit => "-aqm 'Add twitter bootstrap'"
 end
-run "echo '\n@import bootstrap' >> app/assets/stylesheets/application.sass"
-
-git :add => '.'
-git :commit => "-aqm 'Add twitter bootstrap'"
 
