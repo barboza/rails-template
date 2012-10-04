7c9945e86112dbab468eb0d58f3bdf5d86680f82
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-03 23:01:31 -0300

    solved bug into google analytics partial

--- partials/_google_analytics.rb
+++ partials/_google_analytics.rb
@@ -1,6 +1,6 @@
 puts "Configuring the Google Analytics...".magenta
 
-gsub_file 'config/environments/production.rb', "RailsTemplateTest::Application.configure do", "RailsTemplateTest::Application.configure do
+gsub_file 'config/environments/production.rb', "::Application.configure do", "::Application.configure do
   # Google Analytics
   config.middleware.use Rack::GoogleAnalytics, :tracker => 'YOUR-ANALYTICS-CODE'
 "
