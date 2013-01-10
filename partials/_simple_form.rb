51c4083169b416b29cffc6acbe010becf426b6ca
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-10 14:46:43 -0200

    Removes dependence of SimpleForm with bootstrap (Issue #17)

--- partials/_simple_form.rb
+++ partials/_simple_form.rb
@@ -2,7 +2,7 @@ if @use_devise_and_omniauth or would_you_like? "Do you want use the simple_form?
   gsub_file 'Gemfile', /#gem 'simple_form/, "gem 'simple_form"
 
   after_bundler do
-    if @use_devise_and_omniauth or would_you_like? "Do you want use the simple_form with twitter bootstrap? [y,n]".red
+    if would_you_like? "Do you want use the simple_form with twitter bootstrap? [y,n]".red
       generate 'simple_form:install --bootstrap'
     else
       generate 'simple_form:install'
