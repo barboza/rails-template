e301e4f1fe81339dd99b7750f88cdf595a8d2c15
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-09 23:17:55 -0200

    Removes devise locales in pt-br (Closes #22)

--- partials/_locale.rb
+++ partials/_locale.rb
@@ -1,16 +1,7 @@
 puts "Locales ...".magenta
 
-@use_locale_pt = would_you_like? "Use pt-BR locales? [y,n]".red
-
-if @use_locale_pt
-  copy_static_file 'config/locales/devise.pt-BR.yml'
-  git :add => '.'
-  git :commit => "-aqm 'Added default locale for pt-BR'"
-end
-
-
 copy_static_file 'config/locales/devise.views.en.yml'
 copy_static_file 'config/locales/devise.simple_form.en.yml'
 git :add => '.'
-git :commit => "-aqm 'Added default locales for en'"
+git :commit => "-aqm 'Adds default devise locales for EN'"
 
