768370d7eb4f6cdc73087779790dbf2f5fc2a2b4
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-10 00:02:39 -0200

    Removes deprecated find_by_* methods (Close #21)

--- files/app/controllers/users/omniauth_callbacks_controller.rb
+++ files/app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def action_missing(provider)
     omniauth = env["omniauth.auth"]
     if current_user
-      current_user.authorizations.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+      current_user.authorizations.where(provider: omniauth['provider'], uid: omniauth['uid']).first_or_create
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
       redirect_to :back
     else
-      authorization = Authorization.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+      authorization = Authorization.where(provider: omniauth['provider'], uid: omniauth['uid']).first
       if authorization
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
         sign_in_and_redirect(authorization.user, :event => :authentication)
