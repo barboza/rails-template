4661fa2a67650279a945b27749400c1d480ae54d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-06-13 11:13:08 -0300

    Configure Devise helper for specs controllers

--- partials/_rspec.rb
+++ partials/_rspec.rb
@@ -13,6 +13,20 @@ after_bundler do
   generate 'machinist:install'
   git :add => '.'
   git :commit => "-aqm 'Configure RSpec'"
+
+  if prefs[:devise]
+     configs = <<CONFIGS
+
+  config.include Devise::TestHelpers, type: :controller
+CONFIGS
+
+    in_root do
+      inject_into_file 'spec/spec_helper.rb', configs, {after: "do |config|", verbose: false}
+    end
+
+    git :add => '.'
+    git :commit => "-aqm 'Configure Devise helper for specs controllers'"
+  end
 end
 
 puts "\n"
