24bac61b207a8012cb8e4f1c942643a73e7af6ee
Josemar Luedke <josemarluedke@gmail.com>
2014-02-14 14:49:48 -0200

    Change foundation gem to use foundation 5

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -8,13 +8,13 @@ git :commit => "-aqm 'Rename application.css to application.sass'"
 
 if prefer :frontend, 'bootstrap'
   gsub_file 'Gemfile', /#gem 'bootstrap-sass/, "gem 'bootstrap-sass"
-  gsub_file 'Gemfile', /\n  #gem 'zurb-foundation'/, ""
+  gsub_file 'Gemfile', /\n  #gem 'foundation-rails'/, ""
   run "echo '\n@import bootstrap' >> app/assets/stylesheets/application.sass"
   git :add => '.'
   git :commit => "-aqm 'Add twitter bootstrap'"
 
 elsif prefer :frontend, 'foundation'
-  gsub_file 'Gemfile', /#gem 'zurb-foundation/, "gem 'zurb-foundation"
+  gsub_file 'Gemfile', /#gem 'foundation-rails/, "gem 'foundation-rails"
   gsub_file 'Gemfile', /\n  #gem 'bootstrap-sass'/, ""
   run "echo '\n@import foundation' >> app/assets/stylesheets/application.sass"
   git :add => '.'
@@ -22,7 +22,7 @@ elsif prefer :frontend, 'foundation'
 
 else
   gsub_file 'Gemfile', /\n  #gem 'bootstrap-sass'/, ""
-  gsub_file 'Gemfile', /\n  #gem 'zurb-foundation'/, ""
+  gsub_file 'Gemfile', /\n  #gem 'foundation-rails'/, ""
   git :add => '.'
   git :commit => "-aqm 'Remove bootstrap-sass and zurb-foundation from Gemfile'"
 end
