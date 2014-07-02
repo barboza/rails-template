442b31b3cc5d98fc57c165792f3e3235185f748b
Rafael Barboza <barboza.cardoso@gmail.com>
2014-07-02 18:21:24 -0300

    Fix the route syntax

--- partials/_home_page.rb
+++ partials/_home_page.rb
@@ -2,7 +2,7 @@ puts "Adding the Home Page... ".magenta
 
 after_bundler do
   generate :controller, "Home index"
-  gsub_file 'config/routes.rb', /get \"home\/index\"/, 'root :to => "home#index"'
+  gsub_file 'config/routes.rb', /get \'home\/index\'/, 'root :to => "home#index"'
   git :add => '.'
   git :commit => "-aqm 'Add Home Page'"
 end
